e992cc3dfc74553dde55f452f24a7806964bd887
NicolasEiriz <eiriznicolas@gmail.com>
2023-01-18 18:02:54 -0300

    create a function that play one time rock paper scissors againts the computer

--- main.js
+++ main.js
@@ -15,4 +15,23 @@ function getComputerChoice(){
 
 }
 
-getComputerChoice()
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+function playRockPaperScissors(playerSelection){
+  let playerChoice = playerSelection.toLowerCase()
+
+  let computerChoice = getComputerChoice().toLowerCase()
+  
+  if(computerChoice === 'rock' &&  playerChoice === 'scissors' || computerChoice === 'paper' && playerChoice === 'rock' || computerChoice === 'scissors' && playerChoice === 'paper'){
+    return `You Lose! ${computerChoice} beats ${playerChoice}`
+  } else if(playerChoice === computerChoice){return 'Its a tie!'} 
+
+  else{
+    return `You Win! ${playerChoice} beats ${computerChoice}`
+  }
+}
+
+
+
+
